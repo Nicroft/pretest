8814ca515a591c5c33c7985b0d5131fe57f84a7b
Nicroft <ortiznicolas28@gmail.com>
2022-03-19 11:38:47 -0500

    Update PretestApplicationTests.java

--- src/test/java/com/tdea/pretest/PretestApplicationTests.java
+++ src/test/java/com/tdea/pretest/PretestApplicationTests.java
@@ -2,10 +2,19 @@ package com.tdea.pretest;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 class PretestApplicationTests {
 
     @Test
-    void contextLoads() {
+    public void isPassword() {
+
+        String dbPassword = "pass";
+        String password = "pass";
+
+        assertTrue(UserUtils.isPasswordEqual(dbPassword,password));
     }
+
+
 
 }
